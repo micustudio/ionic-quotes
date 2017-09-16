3bfbbe51d1b100d97fcdff90f64eb4a7d635d06f
Michael <micumieu@gmail.com>
2017-09-16 01:05:36 -0700

    Completed side menu and settings page.

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -3,6 +3,8 @@ import { ModalController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 import { QuotesService } from '../../services/quotes';
 import { QuotePage } from '../quote/quote';
+import { SettingsService } from '../../services/settings';
+
 
 @Component({
   selector: 'page-favorites',
@@ -12,7 +14,8 @@ export class FavoritesPage {
   quotes: Quote[];
 
   constructor (private quotesService: QuotesService,
-              private modalCtrl: ModalController) {}
+               private modalCtrl: ModalController,
+               private settingsService: SettingsService ) {}
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
@@ -23,13 +26,22 @@ export class FavoritesPage {
     modal.present();
     modal.onDidDismiss((remove: boolean) => {
       if (remove) {
-        this.quotesService.removeQuoteFromFavorites(quote);
-        // this.quotes = this.quotesService.getFavoriteQuotes();
-        const position = this.quotes.findIndex( (quoteEl: Quote) => {
-          return quoteEl.id == quote.id;
-          });
-        this.quotes.splice(position, 1);
+        this.onRemoveFromFavorites(quote);
       }
     });
   }
+
+  onRemoveFromFavorites(quote: Quote){
+    this.quotesService.removeQuoteFromFavorites(quote);
+    // this.quotes = this.quotesService.getFavoriteQuotes();
+    const position = this.quotes.findIndex( (quoteEl: Quote) => {
+      return quoteEl.id == quote.id;
+      });
+    this.quotes.splice(position, 1);
+  }
+
+  getBackground() {
+    return this.settingsService.isAltBackground() ? 'altQuoteBackground' : 'quoteBackground';
+  }
+
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SettingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { SettingsService } from '../../services/settings';
 
-@IonicPage()
+import { Toggle } from 'ionic-angular';
+
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private settingsService: SettingsService) {}
+
+  onToggle(toggle: Toggle) {
+    console.log(toggle);
+    this.settingsService.setBackground(toggle.checked);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+  checkAltBackground() {
+    return this.settingsService.isAltBackground();
   }
 
 }
